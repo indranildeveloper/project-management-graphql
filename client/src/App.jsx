b17ccab49fd7e06745c5e364ae73f3366e323238
Indranil Halder <indranilhalder.dev@gmail.com>
2023-02-03 12:54:39 +0530

    Bug fix in client

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -27,7 +27,8 @@ const cache = new InMemoryCache({
 });
 
 const client = new ApolloClient({
-  uri: "http://localhost:8000/graphql",
+  // uri: "http://localhost:8000/graphql",
+  uri: "/graphql",
   cache,
 });
 
